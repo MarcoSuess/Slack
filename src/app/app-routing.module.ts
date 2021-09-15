55268fccb03cbfc06d45e28314971b15d4616126
Max Kraemer <maxkraemer@Maxs-MacBook-Air.local>
2021-09-15 21:22:04 +0200

    content component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChannelsComponent } from './channels/channels.component';
+import { ContentComponent } from './content/content.component';
 import { LogInComponent } from './log-in/log-in.component';
 
 
@@ -8,6 +9,7 @@ import { LogInComponent } from './log-in/log-in.component';
 const routes: Routes = [
 
   {path: 'channels', component: ChannelsComponent},
+  {path: 'content', component: ContentComponent},
   {path: '', component: LogInComponent},
 
 ];
